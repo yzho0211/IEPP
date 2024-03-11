9e7ef60f0098ccd8f043a8f51ba4056fd2d8afb0
Yuexuan <zhouyuexuan17@gmail.com>
2024-03-11 14:21:57 +1100

    add the web file

---
+++ sunsavvy/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path 
+from . import views
+
+#URL Config
+urlpatterns = [
+    path('', views.home, name= "home"),
+    path('cloth_style/', views.cloth_style, name= "cloth_style"),
+    path('sunscreen_safety/', views.sunscreen_safety, name= "sunscreen_safety"),
+    path('uv_impact/', views.uv_impact, name= "uv_impact")
+]
