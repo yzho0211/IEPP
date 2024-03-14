0718d24ecd8188f634f6bbca9c710844689ac2f0
yzho0211 <120432410+yzho0211@users.noreply.github.com>
2024-03-14 13:16:21 +1100

    Update settings.py

--- web_project/settings.py
+++ web_project/settings.py
@@ -121,7 +121,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 
 # Add the following lines if they're not already present
 STATICFILES_DIRS = [
