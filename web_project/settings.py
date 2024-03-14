6ab5f8e9cad815171995c9f26fe7f43fa62deff5
yzho0211 <120432410+yzho0211@users.noreply.github.com>
2024-03-15 01:14:46 +1100

    Update settings.py

--- web_project/settings.py
+++ web_project/settings.py
@@ -146,12 +146,12 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # core/settings.py
 
-DEFAULT_FILE_STORAGE = 'core.azure_storage.AzureMediaStorage'
-STATICFILES_STORAGE = 'core.azure_storage.AzureStaticStorage'
+DEFAULT_FILE_STORAGE = 'web_project.azure_storage.AzureMediaStorage'
+STATICFILES_STORAGE = 'web_project.azure_storage.AzureStaticStorage'
 
 AZURE_ACCOUNT_NAME = os.getenv('storage444ie')
 AZURE_ACCOUNT_KEY = os.getenv('5SG8H2Yc9hfL9FcAYsGb+nJzVZ5zsy7Zl4dkCVI3gCYQGI6jBwf1r+oyiXcdE6QePjoS89mzi4bR+AStI7BXmw==')
-AZURE_CUSTOM_DOMAIN = f'storage444ie.blob.core.windows.net'
+AZURE_CUSTOM_DOMAIN = f'storage444ie.blob.web_project.windows.net'
 
 STATIC_URL = f'https://sunsavvvy.azurewebsites.net/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
